22bbb61c8523e4cc8acad51032681c6dc467c798
Zhao, Yujie <yujie.zhao@merck.com>
2023-01-24 15:28:31 -0500

    remove `subset`

--- R/getCutDateForCount.R
+++ R/getCutDateForCount.R
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#' @importFrom dplyr ungroup select mutate filter arrange subset last
+#' @importFrom dplyr ungroup select mutate filter arrange last
 NULL
 #' Get Date at Which an Event Count is Reached
 #'
