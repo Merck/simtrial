8f5c9146a84d6750c7b536a1825f811d3889992d
Ling, Li Li <li.li.ling@merck.com>
2023-03-15 02:21:02 -0400

    "Experimental" -> "experimental"

--- tests/testthat/test-independent_test_tenFH.R
+++ tests/testthat/test-independent_test_tenFH.R
@@ -15,7 +15,7 @@ testthat::test_that("the Z values match with the correspondings in tenFH",{
   survMisc::comp(fit, p= sapply(wt, function(x){x[1]}), q= sapply(wt, function(x){x[2]}))
   tst.rslt <- attr(fit, 'lrt')
   z1=tst.rslt$Z
-  a2 <- y %>% counting_process(arm="Experimental")
+  a2 <- y %>% counting_process(arm="experimental")
   aa=tenFH(a2,rg=tibble(rho=c(0,0,1,1),gamma=c(0,1,0,1)))
   z2=aa$Z
   expect_equal(c(z1[1],z1[7:9]),z2,tolerance = 0.00001)
