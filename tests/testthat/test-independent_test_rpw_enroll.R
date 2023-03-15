adde1e584bbcfa70f430550cafed5669b9dad023
Ling, Li Li <li.li.ling@merck.com>
2023-03-14 23:07:09 -0400

    enroll_rate -> enroll_rate

--- tests/testthat/test-independent_test_rpw_enroll.R
+++ tests/testthat/test-independent_test_rpw_enroll.R
@@ -1,21 +1,21 @@
 test_that("rpw_enroll handles 0 enrollment rate properly for 1st enrollment period (with duration of 1 time unit)", {
   n <- 50
-  enrollRates <- tibble(duration = c(1, 2), rate = c(0, 5))
-  x <- rpw_enroll(n=n, enrollRates=enrollRates)
+  enroll_rate <- tibble(duration = c(1, 2), rate = c(0, 5))
+  x <- rpw_enroll(n=n, enroll_rate=enroll_rate)
 
-  expect_gt(x[1],enrollRates$duration[1])
+  expect_gt(x[1],enroll_rate$duration[1])
 })
 
 test_that("rpw_enroll handles 0 enrollment rate properly for final enrollment period", {
 
   set.seed(123)
   n <- 50
-  enrollRates <- tibble(duration = c(1, 2), rate = c(10, 0))
-  expect_error(rpw_enroll(n=n, enrollRates=enrollRates))
+  enroll_rate <- tibble(duration = c(1, 2), rate = c(10, 0))
+  expect_error(rpw_enroll(n=n, enroll_rate=enroll_rate))
 
   n <- 5
-  enrollRates <- tibble(duration = 1, rate = 0)
-  expect_error(rpw_enroll(n=n, enrollRates=enrollRates))
+  enroll_rate <- tibble(duration = 1, rate = 0)
+  expect_error(rpw_enroll(n=n, enroll_rate=enroll_rate))
 
 })
 
