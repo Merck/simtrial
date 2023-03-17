55500271e9d34f4e5673284ed9cf9f420d1e2f8d
Zhao, Yujie <yujie.zhao@merck.com>
2023-03-17 16:33:21 -0400

    fix test coverage check

--- tests/testthat/test-independent_test_wlr.R
+++ tests/testthat/test-independent_test_wlr.R
@@ -16,7 +16,7 @@ testthat::test_that("the Z values match with the correspondings in wlr",{
   tst.rslt <- attr(fit, 'lrt')
   z1=tst.rslt$Z
   a2 <- y %>% counting_process(arm="Experimental")
-  aa=tenFH(a2,rg=tibble(rho=c(0,0,1,1),gamma=c(0,1,0,1)))
+  aa=wlr(a2,rg=tibble(rho=c(0,0,1,1),gamma=c(0,1,0,1)))
   z2=aa$Z
   expect_equal(c(z1[1],z1[7:9]),z2,tolerance = 0.00001)
 })
