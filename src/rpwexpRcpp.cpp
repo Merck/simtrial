9a53f396e5256c06710a6f626a3ae245289311cd
Nan Xiao <me@nanx.me>
2023-12-05 09:55:35 -0500

    Format C++ code with clang-format

--- src/rpwexpRcpp.cpp
+++ src/rpwexpRcpp.cpp
@@ -9,8 +9,8 @@ using namespace Rcpp;
 //'
 //' @export
 // [[Rcpp::export]]
-NumericVector rpwexpRcpp(int n,
-                      DataFrame fail_rate) {
+NumericVector rpwexpRcpp(int n, DataFrame fail_rate)
+{
   NumericVector duration = fail_rate["duration"];
   NumericVector rate = fail_rate["rate"];
   int n_rates = duration.size();
@@ -18,38 +18,51 @@ NumericVector rpwexpRcpp(int n,
   // Initialize failure times to Inf
   NumericVector times(n, R_PosInf);
 
-  if (n_rates == 1) {
-    if (rate[0] != 0) {
-      times = rexp(n, rate[0]); // generate exponential failure time if non-0 failure rate
+  if (n_rates == 1)
+  {
+    if (rate[0] != 0)
+    {
+      times = rexp(n, rate[0]); // Generate exponential failure time if non-zero failure rate
     }
-  } else {
-    double starttime = 0; // start of first failure rate interval
-    LogicalVector indx(n, 1); // index for event times not yet reached
-    int nindx = n; // number of event times left to generate
+  }
+  else
+  {
+    double starttime = 0;     // Start of first failure rate interval
+    LogicalVector indx(n, 1); // Index for event times not yet reached
+    int nindx = n;            // Number of event times left to generate
 
-    for (int i = 0; i < n_rates; i++) {
-      if (is_false(any(indx))) break; // stop if finished
-      if (rate[i] == 0) {
-        NumericVector temp(nindx, R_PosInf); // set failure time to Inf for inveral i if 0 fail rate
+    for (int i = 0; i < n_rates; i++)
+    {
+      if (is_false(any(indx)))
+        break; // stop if finished
+      if (rate[i] == 0)
+      {
+        NumericVector temp(nindx, R_PosInf); // Set failure time to Inf for interval i if 0 fail rate
         times[indx] = temp;
-      } else {
-        NumericVector temp = rexp(nindx, rate[i]); // generate exponential failure time for interval i if non-0 faiurel rate
+      }
+      else
+      {
+        NumericVector temp = rexp(nindx, rate[i]); // Generate exponential failure time for interval i if non-0 failure rate
         int p = 0;
-        for (int j = 0; j < n; j++) {
-          if (indx[j]) times[j] = temp[p++] + starttime;
+        for (int j = 0; j < n; j++)
+        {
+          if (indx[j])
+            times[j] = temp[p++] + starttime;
         }
       }
 
-      if (i < n_rates) { // skip this for last interval as all remaining times are generated there
-        starttime += duration[i]; // update start time for next interval
-        for (int j = 0; j < n; j++) {
-          if (indx[j] == 1 && times[j] <= starttime) {
-            indx[j] = 0; // update index of event times not yet reached
-            nindx--; // update number of event times left to generate
+      if (i < n_rates)
+      {                           // Skip this for last interval as all remaining times are generated there
+        starttime += duration[i]; // Update start time for next interval
+        for (int j = 0; j < n; j++)
+        {
+          if (indx[j] == 1 && times[j] <= starttime)
+          {
+            indx[j] = 0; // Update index of event times not yet reached
+            nindx--;     // Update number of event times left to generate
           }
         }
       }
-
     }
   }
 
--- src/rpwexpinvRcpp.cpp
+++ src/rpwexpinvRcpp.cpp
@@ -10,9 +10,8 @@ using namespace Rcpp;
 //'
 //' @export
 // [[Rcpp::export]]
-NumericVector rpwexpinvRcpp(int n,
-                            DataFrame fail_rate) {
-
+NumericVector rpwexpinvRcpp(int n, DataFrame fail_rate)
+{
   NumericVector duration = fail_rate["duration"];
   NumericVector rate = fail_rate["rate"];
   int n_rates = duration.size();
@@ -23,14 +22,16 @@ NumericVector rpwexpinvRcpp(int n,
   // Get number of piecewise rates
   NumericVector cumTime(n_rates);
   NumericVector cumHaz(n_rates);
-  for (int i = 1; i < n_rates; i++) {
-    cumTime[i] = cumTime[i-1] + duration[i-1];
-    cumHaz[i] = cumHaz[i-1] + duration[i-1] * rate[i-1];
+  for (int i = 1; i < n_rates; i++)
+  {
+    cumTime[i] = cumTime[i - 1] + duration[i - 1];
+    cumHaz[i] = cumHaz[i - 1] + duration[i - 1] * rate[i - 1];
   }
 
   NumericVector::iterator pos;
   int j;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     pos = std::upper_bound(cumHaz.begin(), cumHaz.end(), times[i]);
     j = pos - cumHaz.begin() - 1;
     times[i] = cumTime[j] + (times[i] - cumHaz[j]) / rate[j];
