957c3775055c7734693b747b3366ee599a3a2511
Nan Xiao <me@nanx.me>
2023-12-06 16:16:12 -0500

    Avoid using `:::` in tests

--- tests/testthat/test-independent_test_rpwexp_inverse_cdf_cpp.R
+++ tests/testthat/test-independent_test_rpwexp_inverse_cdf_cpp.R
@@ -1,6 +1,6 @@
 test_that("rpwexp_inverse_cdf_cpp handles 0 fail_rate for final period", {
   # 0 failure rate for last period
-  s <- simtrial:::rpwexp_inverse_cdf_cpp(
+  s <- rpwexp_inverse_cdf_cpp(
     n = 100,
     fail_rate = data.frame(duration = c(0, 2), rate = c(5, 0))
   )
@@ -10,7 +10,7 @@ test_that("rpwexp_inverse_cdf_cpp handles 0 fail_rate for final period", {
 
 testthat::test_that("rpwexp_inverse_cdf_cpp handles 0 fail rate properly for one period", {
   # 0 failure rate
-  s <- simtrial:::rpwexp_inverse_cdf_cpp(
+  s <- rpwexp_inverse_cdf_cpp(
     n = 100,
     fail_rate = data.frame(duration = c(1), rate = c(0))
   )
@@ -21,7 +21,7 @@ testthat::test_that("rpwexp_inverse_cdf_cpp handles 0 fail rate properly for one
 
 testthat::test_that("rpwexp_inverse_cdf_cpp handles 0 fail rate properly for multiple periods", {
   # 0 failure rate for 1st period (with duration of 1 time unit)
-  s <- simtrial:::rpwexp_inverse_cdf_cpp(
+  s <- rpwexp_inverse_cdf_cpp(
     n = 100,
     fail_rate = data.frame(duration = c(1, 2), rate = c(0, 5))
   )
