d4998ccaa86497010c2e642d7ba47b6730a8a539
Zhao, Yujie <yujie.zhao@merck.com>
2024-01-04 14:49:27 -0500

    add a title line

--- R/get_analysis_date.R
+++ R/get_analysis_date.R
@@ -16,6 +16,9 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+#' Derive analysis date for interim/final analysis given multiple conditions.
+#'
 #' To obtain the analysis date, consider the following multiple conditions:
 #' Condition 1: The planned calendar time for analysis.
 #' Condition 2: The targeted events, encompassing both overall population
