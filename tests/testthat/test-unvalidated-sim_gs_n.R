c8629b3b696e3cde02faf22e354acecabbfef690
John Blischak <jdblischak@gmail.com>
2024-02-23 11:30:25 -0500

    Move sim_gs_n() test file scope code to helper functions

--- tests/testthat/test-unvalidated-sim_gs_n.R
+++ tests/testthat/test-unvalidated-sim_gs_n.R
@@ -1,73 +1,16 @@
 # 2024-02-22: Converted `example("sim_gs_n")` to tests from commit 306de0d
 # https://github.com/Merck/simtrial/tree/306de0dbe380fdb1e906a59f34bf3871d3ee5312
 
-# parameters for enrollment
-enroll_rampup_duration <- 4 # duration for enrollment ramp up
-enroll_duration <- 16       # total enrollment duration
-enroll_rate <- gsDesign2::define_enroll_rate(
-  duration = c(enroll_rampup_duration,
-               enroll_duration - enroll_rampup_duration),
-  rate = c(10, 30)
-)
-# parameters for treatment effect
-delay_effect_duration <- 3  # delay treatment effect in months
-median_col <- 9             # survival median of the control arm
-median_exp <- c(9, 14)      # survival median of the experimental arm
-dropout_rate <- 0.001
-fail_rate <- gsDesign2::define_fail_rate(
-  duration = c(delay_effect_duration, 100),
-  fail_rate = log(2) /  median_col,
-  hr = median_col / median_exp,
-  dropout_rate = dropout_rate
-)
-# other related parameters
-alpha <- 0.025              # type I error
-beta <- 0.1                 # type II error
-ratio <- 1                  # randomization ratio (exp:col)
-# Define cuttings of 2 IAs and 1 FA
-# IA1
-# The 1st interim analysis will occur at the later of the following 3 conditions:
-# - At least 20 months have passed since the start of the study
-# - At least 100 events have occurred
-# - At least 20 months have elapsed after enrolling 200/400 subjects, with a
-#   minimum of 20 months follow-up
-# However, if events accumulation is slow, we will wait for a maximum of 24 months.
-ia1 <- create_cutting(
-  planned_calendar_time = 20,
-  target_event_overall = 100,
-  max_extension_for_target_event = 24,
-  min_n_overall = 200,
-  min_followup = 20
-)
-# IA2
-# The 2nd interim analysis will occur at the later of the following 3 conditions:
-# - At least 32 months have passed since the start of the study
-# - At least 250 events have occurred
-# - At least 10 months after IA1
-# However, if events accumulation is slow, we will wait for a maximum of 34 months.
-ia2 <- create_cutting(
-  planned_calendar_time = 32,
-  target_event_overall = 200,
-  max_extension_for_target_event = 34,
-  min_time_after_previous_analysis = 10
-)
-# FA
-# The final analysis will occur at the later of the following 2 conditions:
-# - At least 45 months have passed since the start of the study
-# - At least 300 events have occurred
-fa <- create_cutting(
-  planned_calendar_time = 45,
-  target_event_overall = 350
-)
+# See helper-sim_gs_n.R for helper functions
 
 test_that("Test 1: regular logrank test", {
   observed <- sim_gs_n(
     n_sim = 3,
     sample_size = 400,
-    enroll_rate = enroll_rate,
-    fail_rate = fail_rate,
+    enroll_rate = test_enroll_rate(),
+    fail_rate = test_fail_rate(),
     test = wlr,
-    cutting = list(ia1 = ia1, ia2 = ia2, fa = fa),
+    cutting = test_cutting(),
     seed = 2024,
     weight = fh(rho = 0, gamma = 0)
   )
@@ -90,10 +33,10 @@ test_that("Test 2: weighted logrank test by FH(0, 0.5)", {
   observed <- sim_gs_n(
     n_sim = 3,
     sample_size = 400,
-    enroll_rate = enroll_rate,
-    fail_rate = fail_rate,
+    enroll_rate = test_enroll_rate(),
+    fail_rate = test_fail_rate(),
     test = wlr,
-    cutting = list(ia1 = ia1, ia2 = ia2, fa = fa),
+    cutting = test_cutting(),
     seed = 2024,
     weight = fh(rho = 0, gamma = 0.5)
   )
@@ -116,10 +59,10 @@ test_that("Test 3: weighted logrank test by MB(6)", {
   observed <- sim_gs_n(
     n_sim = 3,
     sample_size = 400,
-    enroll_rate = enroll_rate,
-    fail_rate = fail_rate,
+    enroll_rate = test_enroll_rate(),
+    fail_rate = test_fail_rate(),
     test = wlr,
-    cutting = list(ia1 = ia1, ia2 = ia2, fa = fa),
+    cutting = test_cutting(),
     seed = 2024,
     weight = mb(delay = 3)
   )
@@ -140,10 +83,10 @@ test_that("Test 4: weighted logrank test by early zero (6)", {
   observed <- sim_gs_n(
     n_sim = 3,
     sample_size = 400,
-    enroll_rate = enroll_rate,
-    fail_rate = fail_rate,
+    enroll_rate = test_enroll_rate(),
+    fail_rate = test_fail_rate(),
     test = wlr,
-    cutting = list(ia1 = ia1, ia2 = ia2, fa = fa),
+    cutting = test_cutting(),
     seed = 2024,
     weight = early_zero(6)
   )
@@ -164,10 +107,10 @@ test_that("Test 5: RMST", {
   observed <- sim_gs_n(
     n_sim = 3,
     sample_size = 400,
-    enroll_rate = enroll_rate,
-    fail_rate = fail_rate,
+    enroll_rate = test_enroll_rate(),
+    fail_rate = test_fail_rate(),
     test = rmst,
-    cutting = list(ia1 = ia1, ia2 = ia2, fa = fa),
+    cutting = test_cutting(),
     seed = 2024,
     tau = 20
   )
@@ -203,10 +146,10 @@ test_that("Test 6: maxcombo (FH(0,0) + FH(0, 0.5))", {
   observed <- sim_gs_n(
     n_sim = 3,
     sample_size = 400,
-    enroll_rate = enroll_rate,
-    fail_rate = fail_rate,
+    enroll_rate = test_enroll_rate(),
+    fail_rate = test_fail_rate(),
     test = maxcombo,
-    cutting = list(ia1 = ia1, ia2 = ia2, fa = fa),
+    cutting = test_cutting(),
     seed = 2024,
     rho = c(0, 0),
     gamma = c(0, 0.5)
