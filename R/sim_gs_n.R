b9adb98bb0949097d271cf0ff5174e67200d22ab
John Blischak <jdblischak@gmail.com>
2024-03-06 20:52:03 -0500

    Add create_cutting_test()

--- R/sim_gs_n.R
+++ R/sim_gs_n.R
@@ -324,3 +324,42 @@ create_cutting <- function(...) {
     get_analysis_date(data, ...)
   }
 }
+
+#' Create a cutting test function
+#'
+#' Create a cutting test function for use with [sim_gs_n()]
+#'
+#' @param test A test function such as [wlr()], [maxcombo()], or [rmst()]
+#' @param ... Arguments passed to the cutting test function
+#'
+#' @return A function that accepts a data frame of simulated trial data and
+#'   returns a test result
+#'
+#' @export
+#'
+#' @seealso [sim_gs_n()], [create_cutting()]
+#'
+#' @examples
+#' # Simulate trial data
+#' trial_data <- sim_pw_surv()
+#'
+#' # Cut after 150 events
+#' trial_data_cut <- cut_data_by_event(trial_data, 150)
+#'
+#' # Create a cutting test function that can be used by sim_gs_n()
+#' regular_logrank_test <- create_cutting_test(wlr, weight = fh(rho = 0, gamma = 0))
+#'
+#' # Test the cutting
+#' regular_logrank_test(trial_data_cut)
+#'
+#' # The results are the same as directly calling the function
+#' stopifnot(all.equal(
+#'   regular_logrank_test(trial_data_cut),
+#'   wlr(trial_data_cut, weight = fh(rho = 0, gamma = 0))
+#' ))
+create_cutting_test <- function(test, ...) {
+  stopifnot(is.function(test))
+  function(data) {
+    test(data, ...)
+  }
+}
