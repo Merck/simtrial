c7cfe5867b42fd8667b43422255159a2e026c5ca
Zhao, Yujie <yujie.zhao@merck.com>
2024-04-09 10:18:42 -0400

    unexport `mb_weight` and make its output as list

--- R/mb_weight.R
+++ R/mb_weight.R
@@ -31,79 +31,8 @@
 #'   Set `delay = Inf`, `w_max = 2` to be consistent with recommendation of
 #'   Magirr (2021).
 #'
-#' @return A data frame. The column `mb_weight` contains the weights for the
-#'   Magirr-Burman weighted logrank test for the data in `x`.
-#'
-#' @details
-#' Magirr and Burman (2019) proposed a weighted logrank test to have better
-#' power than the logrank test when the treatment effect is delayed,
-#' but to still maintain good power under a proportional hazards assumption.
-#' In Magirr (2021), (the equivalent of) a maximum weight was proposed
-#' as opposed to a fixed time duration over which weights would increase.
-#' The weights for some early interval specified by the user are the inverse
-#' of the combined treatment group empirical survival distribution; see details.
-#' After this initial period, weights are constant at the maximum of the
-#' previous weights. Another advantage of the test is that under strong
-#' null hypothesis that the underlying survival in the control group is
-#' greater than or equal to underlying survival in the experimental group,
-#' Type I error is controlled as the specified level.
-#'
-#' We define \eqn{t^*} to be the input variable `delay`.
-#' This specifies an initial period during which weights increase.
-#' We also set a maximum weight \eqn{w_{\max}}.
-#' To define specific weights, we let \eqn{S(t)} denote the Kaplan-Meier
-#' survival estimate at time \eqn{t} for the combined data
-#' (control plus experimental treatment groups).
-#' The weight at time \eqn{t} is then defined as
-#' \deqn{w(t)=\min(w_{\max}, S(\min(t, t^*))^{-1}).}
-#'
-#' @references
-#' Magirr, Dominic, and Carl‐Fredrik Burman. 2019.
-#' "Modestly weighted logrank tests."
-#' _Statistics in Medicine_ 38 (20): 3782--3790.
-#'
-#' Magirr, Dominic. 2021.
-#' "Non‐proportional hazards in immuno‐oncology: Is an old perspective needed?"
-#' _Pharmaceutical Statistics_ 20 (3): 512--527.
-#'
+#' @return A list.
 #' @importFrom data.table ":=" as.data.table data.table fifelse merge.data.table setDF
-#'
-#' @export
-#'
-#' @examples
-#' library(dplyr)
-#'
-#' # Use default enrollment and event rates at cut at 100 events
-#' # For transparency, may be good to set either `delay` or `w_max` to `Inf`
-#' x <- sim_pw_surv(n = 200) |>
-#'   cut_data_by_event(125) |>
-#'   counting_process(arm = "experimental")
-#'
-#' # Example 1
-#' # Compute Magirr-Burman weights with `delay = 6`
-#' ZMB <- x |>
-#'   mb_weight(delay = 6, w_max = Inf) |>
-#'   summarize(
-#'     S = sum(o_minus_e * mb_weight),
-#'     V = sum(var_o_minus_e * mb_weight^2),
-#'     z = S / sqrt(V)
-#'   )
-#'
-#' # Compute p-value of modestly weighted logrank of Magirr-Burman
-#' pnorm(ZMB$z)
-#'
-#' # Example 2
-#' # Now compute with maximum weight of 2 as recommended in Magirr, 2021
-#' ZMB2 <- x |>
-#'   mb_weight(delay = Inf, w_max = 2) |>
-#'   summarize(
-#'     S = sum(o_minus_e * mb_weight),
-#'     V = sum(var_o_minus_e * mb_weight^2),
-#'     z = S / sqrt(V)
-#'   )
-#'
-#' # Compute p-value of modestly weighted logrank of Magirr-Burman
-#' pnorm(ZMB2$z)
 mb_weight <- function(x, delay = 4, w_max = Inf) {
   # check input failure rate assumptions
   if (!is.data.frame(x)) {
@@ -139,27 +68,35 @@ mb_weight <- function(x, delay = 4, w_max = Inf) {
     stop("x column names in `mb_weight()` must contain s")
   }
 
+  ans <- list()
+  ans$method <- "WLR"
+  ans$parameter <- paste0("MB(delay = ", delay, ", max_weight = ", w_max, ")")
   # Compute max weight by stratum
   x2 <- as.data.table(x)
   # Make sure you don't lose any stratum!
   tbl_all_stratum <- data.table(stratum = unique(x2$stratum))
 
   # Look only up to delay time
-  ans <- x2[tte <= delay, ]
+  res <- x2[tte <= delay, ]
   # Weight before delay specified as 1/S
-  ans <- ans[, .(max_weight = max(1 / s)), by = "stratum"]
+  res <- res[, .(max_weight = max(1 / s)), by = "stratum"]
   # Get back stratum with no records before delay ends
-  ans <- ans[tbl_all_stratum, on = "stratum"]
+  res <- res[tbl_all_stratum, on = "stratum"]
   # `max_weight` is 1 when there are no records before delay ends
-  ans[, max_weight := fifelse(is.na(max_weight), 1, max_weight)]
+  res[, max_weight := fifelse(is.na(max_weight), 1, max_weight)]
   # Cut off weights at w_max
-  ans[, max_weight := pmin(w_max, max_weight)]
+  res[, max_weight := pmin(w_max, max_weight)]
   # Now merge max_weight back to stratified dataset
-  ans <- merge.data.table(ans, x2, by = "stratum", all = TRUE)
+  res <- merge.data.table(res, x2, by = "stratum", all = TRUE)
   # Weight is min of max_weight and 1/S which will increase up to delay
-  ans[, mb_weight := pmin(max_weight, 1 / s)]
-  ans[, max_weight := NULL]
+  res[, mb_weight := pmin(max_weight, 1 / s)]
+  res[, max_weight := NULL]
 
-  setDF(ans)
+  setDF(res)
+
+  ans$estimate <- sum(res$o_minus_e * res$mb_weight)
+  ans$se <- sqrt(sum(res$var_o_minus_e * res$mb_weight^2))
+  ans$z <- ans$estimate / ans$se
+
   return(ans)
 }
