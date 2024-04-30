4457885fccfea8902d0af1a631774a6f8988b9db
Nan Xiao <me@nanx.me>
2024-04-29 23:00:34 -0400

    Use `data.frame()`, qualify namespace, run styler

--- tests/testthat/test-independent_test_early_zero_weight.R
+++ tests/testthat/test-independent_test_early_zero_weight.R
@@ -1,11 +1,6 @@
-library(dplyr)
-library(gsDesign2)
-library(tibble)
-
-
 test_that("early_zero_weight() with unstratified data", {
   set.seed(123456)
-  early_period = 2
+  early_period <- 2
 
   output <- sim_pw_surv(n = 200) |>
     cut_data_by_event(125) |>
@@ -13,15 +8,15 @@ test_that("early_zero_weight() with unstratified data", {
     early_zero_weight(early_period = early_period)
 
   observed <- output$weight
-  expected <- if_else(output$tte < early_period,0,1)
+  expected <- dplyr::if_else(output$tte < early_period, 0, 1)
   expect_equal(observed, expected)
 })
 
-
 test_that("early_zero_weight() with stratified data when fail_rate is not provided", {
   set.seed(123456)
-  early_period = 2
+  early_period <- 2
   n <- 500
+
   # Two strata
   stratum <- c("Biomarker-positive", "Biomarker-negative")
   prevalence_ratio <- c(0.6, 0.4)
@@ -47,7 +42,7 @@ test_that("early_zero_weight() with stratified data when fail_rate is not provid
   x <- sim_pw_surv(
     n = n, # Sample size
     # Stratified design with prevalence ratio of 6:4
-    stratum = tibble(stratum = stratum, p = prevalence_ratio),
+    stratum = data.frame(stratum = stratum, p = prevalence_ratio),
     # Randomization ratio
     block = c("control", "control", "experimental", "experimental"),
     enroll_rate = enroll_rate, # Enrollment rate
@@ -57,14 +52,17 @@ test_that("early_zero_weight() with stratified data when fail_rate is not provid
     cut_data_by_event(125) |>
     counting_process(arm = "experimental")
 
-  expect_error(early_zero_weight(x, early_period = early_period),"For stratified design to use `early_zero_weight\\(\\)`, `fail_rate` can't be `NULL`.")
+  expect_error(
+    early_zero_weight(x, early_period = early_period),
+    "For stratified design to use `early_zero_weight\\(\\)`, `fail_rate` can't be `NULL`."
+  )
 })
-
 
 test_that("early_zero_weight() with stratified data when fail_rate is not correctly provided", {
   set.seed(123456)
-  early_period = 2
+  early_period <- 2
   n <- 500
+
   # Two strata
   stratum <- c("Biomarker-positive", "Biomarker-negative")
   prevalence_ratio <- c(0.6, 0.4)
@@ -86,18 +84,19 @@ test_that("early_zero_weight() with stratified data when fail_rate is not correc
     hr = c(hr_pos, hr_neg),
     dropout_rate = 0.01
   )
-  fail_rate_wrong <- tibble(
-    stratum = c(rep('Biomarker-positive',3), rep('Biomarker-negative',2)),
+  fail_rate_wrong <- data.frame(
+    stratum = c(rep("Biomarker-positive", 3), rep("Biomarker-negative", 2)),
     duration = c(3, 4, 1000, 4, 1000),
     fil_rate = c(log(2) / c(med_pos, med_pos, med_pos, med_neg, med_neg)),
-    dropout_rate = rep(0.01,5),
+    dropout_rate = rep(0.01, 5),
     hr = c(1, 1, 0.7, 1, 0.8),
+    stringsAsFactors = FALSE
   )
   temp <- to_sim_pw_surv(fail_rate) # Convert the failure rate
   x <- sim_pw_surv(
     n = n, # Sample size
     # Stratified design with prevalence ratio of 6:4
-    stratum = tibble(stratum = stratum, p = prevalence_ratio),
+    stratum = data.frame(stratum = stratum, p = prevalence_ratio),
     # Randomization ratio
     block = c("control", "control", "experimental", "experimental"),
     enroll_rate = enroll_rate, # Enrollment rate
@@ -107,13 +106,15 @@ test_that("early_zero_weight() with stratified data when fail_rate is not correc
     cut_data_by_event(125) |>
     counting_process(arm = "experimental")
 
-  expect_error(early_zero_weight(x, early_period = early_period, fail_rate = fail_rate_wrong),"`early_zero_weight\\(\\)` only allows delayed treatment effect, that is, 2 piece failure rate with HR = 1 at the first period.")
+  expect_error(
+    early_zero_weight(x, early_period = early_period, fail_rate = fail_rate_wrong),
+    "`early_zero_weight\\(\\)` only allows delayed treatment effect, that is, 2 piece failure rate with HR = 1 at the first period."
+  )
 })
-
 
 test_that("early_zero_weight() with stratified data when fail_rate is correctly provided", {
   set.seed(123456)
-  early_period = 2
+  early_period <- 2
   n <- 500
   # Two strata
   stratum <- c("Biomarker-positive", "Biomarker-negative")
@@ -136,18 +137,19 @@ test_that("early_zero_weight() with stratified data when fail_rate is correctly 
     hr = c(hr_pos, hr_neg),
     dropout_rate = 0.01
   )
-  fail_rate_wrong <- tibble(
-    stratum = c(rep('Biomarker-positive',3), rep('Biomarker-negative',2)),
+  fail_rate_wrong <- data.frame(
+    stratum = c(rep("Biomarker-positive", 3), rep("Biomarker-negative", 2)),
     duration = c(3, 4, 1000, 4, 1000),
     fil_rate = c(log(2) / c(med_pos, med_pos, med_pos, med_neg, med_neg)),
-    dropout_rate = rep(0.01,5),
+    dropout_rate = rep(0.01, 5),
     hr = c(1, 1, 0.7, 1, 0.8),
+    stringsAsFactors = FALSE
   )
   temp <- to_sim_pw_surv(fail_rate) # Convert the failure rate
   output <- sim_pw_surv(
     n = n, # Sample size
     # Stratified design with prevalence ratio of 6:4
-    stratum = tibble(stratum = stratum, p = prevalence_ratio),
+    stratum = data.frame(stratum = stratum, p = prevalence_ratio),
     # Randomization ratio
     block = c("control", "control", "experimental", "experimental"),
     enroll_rate = enroll_rate, # Enrollment rate
@@ -159,6 +161,10 @@ test_that("early_zero_weight() with stratified data when fail_rate is correctly 
     early_zero_weight(early_period = early_period, fail_rate = fail_rate)
 
   observed <- output$weight
-  expected <- if_else(output$stratum=='Biomarker-negative',if_else(output$tte<4,0,log(0.8)),if_else(output$tte<3,0,log(0.7)))
+  expected <- dplyr::if_else(
+    output$stratum == "Biomarker-negative",
+    dplyr::if_else(output$tte < 4, 0, log(0.8)),
+    dplyr::if_else(output$tte < 3, 0, log(0.7))
+  )
   expect_equal(observed, expected)
 })
